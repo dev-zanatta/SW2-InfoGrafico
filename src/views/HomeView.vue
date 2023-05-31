71ac35482de8b67930f5f0f3df445520cb3001a4
Gustavo Zanatta <109366419+dev-zanatta@users.noreply.github.com>
2023-05-31 19:56:17 -0300

    Add files via upload

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,23 @@
 </script>
 
 <template>
-  <main>
+  <main style="background-color: white; height: 100vh; width: 100vw">
     <TheWelcome />
+    
+    <div class="teste"></div>
   </main>
 </template>
+
+<style>
+.teste {
+  background-image: url('../img/fundo.png');
+  background-size: contain;
+  background-repeat: no-repeat;
+  width: 70%;
+  height: 700%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: auto;
+  margin-bottom: auto;
+}
+</style>
