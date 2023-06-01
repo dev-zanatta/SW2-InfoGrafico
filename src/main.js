66f033498f26b8eb4f8e0877422d936614c07222
Joao Paiva <joao.paiva@ssys.com.br>
2023-06-01 09:38:29 -0300

    Implementada leftbar

--- src/main.js
+++ src/main.js
@@ -5,10 +5,13 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import vueAwesomeSidebar from 'vue-awesome-sidebar'
+import 'vue-awesome-sidebar/dist/vue-awesome-sidebar.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vueAwesomeSidebar)
 
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,14 @@
-<script setup>
-</script>
-
 <template>
   <main style="background-color: white; height: 100vh; width: 100vw">
-    <TheWelcome />
-    
-    <div class="teste"></div>
+
+    <div class="teste">
+      <VueAwesomeSideBar
+          v-model:miniMenu="miniMenu"
+          v-model:collapsed="collapsed"
+          :menu="testMenu"
+          vueRouterEnabel
+        ></VueAwesomeSideBar>
+    </div>
   </main>
 </template>
 
@@ -22,3 +25,54 @@
   margin-bottom: auto;
 }
 </style>
+
+<script setup>
+  import { ref } from 'vue'
+
+const collapsed = ref(false)
+const miniMenu = ref(false)
+
+const testMenu = [
+  {
+    name: 'Tipo do infográfico',
+    icon: { text: 'home' , class: 'material-icons-outlined' },
+    children: [
+      {
+        href: '/#1',
+        name: 'Grau de sei la o que',
+        icon: { class: 'material-icons-outlined'},
+      },
+      {
+        href: '/#2',
+        name: 'Grau de cornisse'
+      }
+    ],
+  },
+  {
+    name: 'Ano de referência',
+    icon: { text: 'Ano', class: 'material-icons-outlined' },
+    children: [
+      {
+        href: '/#3',
+        name: '2023',
+      },
+      {
+        href: '/#4',
+        name: '2022',
+      },
+      {
+        href: '/#5',
+        name: '2021',
+      },
+      {
+        href: '/#6',
+        name: '2020',
+      },
+    ]
+  },
+  {
+    name: 'Sobre',
+    icon: { text: 'So', class: 'material-icons-outlined' },
+  },
+]
+</script>
